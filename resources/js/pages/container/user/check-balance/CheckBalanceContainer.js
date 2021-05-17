af992e10902f99a4a9b338ec674d017c6cfd5505
Hugo <hugo.zorrilla.93@gmail.com>
2021-05-16 20:46:21 -0400

    interfaz de consultar saldo

--- resources/js/pages/container/user/check-balance/CheckBalanceContainer.js
+++ resources/js/pages/container/user/check-balance/CheckBalanceContainer.js
@@ -7,17 +7,6 @@ const CheckBalanceContainer = () => {
 
 	const handleAddSubmit = (data,e) => {
 		console.log(data)
-		axios.post(`http://localhost:8000/api/transaction/load-wallet`,{		 	
-		 	data:{
-				document:data.document,
-				telephone:data.telephone		
-		 	}
-		}).then(result => {		 
-			alert("Su recarga fu exitosa")			
-		 			 	
-        }).catch(e => {
-            console.log(e.message);
-        });	
 	}
 
 	return(
